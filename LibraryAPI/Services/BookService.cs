9686fe77883a44c42d14e0a427e44897c35902e6
joohxz <joohxz@gmail.com>
2024-09-03 01:54:24 -0300

    Creates the Book Services

---
+++ LibraryAPI/Services/BookService.cs
@@ -0,0 +1,55 @@
+﻿using LibraryAPI.Models;
+
+namespace LibraryAPI.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly List<Book> books = new List<Book>();
+        public Book CreateBook(Book book)
+        {
+            book.Id = Guid.NewGuid();
+            books.Add(book);
+            return book;
+        }
+
+        public bool DeleteBook(Guid id)
+        {
+            var book = GetById(id);
+            if (book != null)
+            {
+                books.Remove(book);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public IEnumerable<Book> GetAll()
+        {
+            return books;
+        }
+
+        public Book GetById(Guid id)
+        {
+            return books.FirstOrDefault( b => b.Id == id);
+        }
+
+        public bool UpdateBook(Guid id, Book updatedBook)
+        {
+            var existingBook = GetById(id);
+            if (existingBook == null)
+            {
+                return false;
+            }
+            existingBook.Author = updatedBook.Author;
+            existingBook.Title = updatedBook.Title;
+            existingBook.Genre = updatedBook.Genre;
+            existingBook.Price = updatedBook.Price;
+            existingBook.Quantity = updatedBook.Quantity;
+            return true;
+
+        }
+    }
+}
---
+++ LibraryAPI/Services/IBookService.cs
@@ -0,0 +1,14 @@
+﻿using LibraryAPI.Models;
+
+namespace LibraryAPI.Services
+{
+    public interface IBookService
+    {
+        Book CreateBook(Book book);
+        Book GetById(Guid id);
+        IEnumerable<Book> GetAll();
+        bool UpdateBook(Guid id);
+        bool DeleteBook(Guid id);
+
+    }
+}
