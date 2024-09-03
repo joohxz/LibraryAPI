e2730a5df2ffe867610af26b5d929385c6a31e36
joohxz <joohxz@gmail.com>
2024-09-02 21:43:07 -0300

    Changes Variable TypeOf

--- LibraryAPI/Models/Book.cs
+++ LibraryAPI/Models/Book.cs
@@ -6,7 +6,7 @@
         public string Title { get; set; }
         public string Author { get; set; }
         public string Gender { get; set; }
-        public string Price { get; set; }
-        public string Quantity { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
     }
 }
