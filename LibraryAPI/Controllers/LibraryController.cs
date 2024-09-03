4d61d0ad536baca5e19d30f6b8f0c3f26317d517
joohxz <joohxz@gmail.com>
2024-09-02 21:33:29 -0300

    Add project files.

---
+++ LibraryAPI/Controllers/LibraryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAPI.Controllers
+{
+    public class LibraryController : LibraryBaseController
+    {
+
+        [HttpGet("test-api")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Get() => Ok("Testando");
+
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetBooks() => Ok("Books");
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public IActionResult CreateBook() => Created();
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult UpdateBook() => NoContent();
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult DeleteBook() => NoContent();
+    }
+}
---
+++ LibraryAPI/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Gender { get; set; }
+        public string Price { get; set; }
+        public string Quantity { get; set; }
+    }
+}
