02a00cd67d684063cb8c011b8529e515bd78fd97
joohxz <joohxz@gmail.com>
2024-09-03 01:53:22 -0300

    Change variable type of 'Id' to 'Guid'

--- LibraryAPI/Models/Book.cs
+++ LibraryAPI/Models/Book.cs
@@ -2,10 +2,10 @@
 {
     public class Book
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
-        public string Gender { get; set; }
+        public string Genre { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
     }
